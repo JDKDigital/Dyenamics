dc729c54d873d665c8d85666eaf2c3d9e4804bc9
FloofHips <61808556+FloofHips@users.noreply.github.com>
2021-04-19 03:06:25 +0000

    Added Glass Blocks

--- src/main/java/cofh/dyenamics/Dyenamics.java
+++ src/main/java/cofh/dyenamics/Dyenamics.java
@@ -1,5 +1,10 @@
 package cofh.dyenamics;
 
+import cofh.dyenamics.common.blocks.DyenamicStainedGlassBlock;
+import cofh.dyenamics.core.util.DyenamicDyeColor;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
@@ -15,37 +20,49 @@ import cofh.dyenamics.core.init.BlockInit;
 import cofh.dyenamics.core.init.EntityInit;
 import cofh.dyenamics.core.init.ItemInit;
 
+import javax.swing.*;
+import java.util.List;
+import java.util.Map;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Dyenamics.MOD_ID)
 public class Dyenamics
 {
-	static {
-		BlockInit.register();
-		ItemInit.register();
-		EntityInit.register();
-	}
-	
+    static {
+        BlockInit.register();
+        ItemInit.register();
+        EntityInit.register();
+    }
+
     public static final Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "dyenamics";
 
     public Dyenamics() {
-    	IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         // Register the setup method for modloading
         bus.addListener(this::commonSetup);
         bus.addListener(this::clientSetup);
         BlockInit.BLOCKS.register(bus);
         ItemInit.ITEMS.register(bus);
         EntityInit.ENTITIES.register(bus);
-        
+
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-    	event.enqueueWork(EntityInit::setup);
+        event.enqueueWork(EntityInit::setup);
     }
-    
+
     private void clientSetup(final FMLClientSetupEvent event) {
-    	RenderingRegistry.registerEntityRenderingHandler(EntityInit.SHEEP.get(), DyenamicSheepRenderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(EntityInit.SHEEP.get(), DyenamicSheepRenderer::new);
+        RenderTypeLookup.setRenderLayer(BlockInit.DYED_BLOCKS.get("peach").get("stained_glass").get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.DYED_BLOCKS.get("aquamarine").get("stained_glass").get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.DYED_BLOCKS.get("fluorescent").get("stained_glass").get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.DYED_BLOCKS.get("mint").get("stained_glass").get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.DYED_BLOCKS.get("maroon").get("stained_glass").get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.DYED_BLOCKS.get("bubblegum").get("stained_glass").get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.DYED_BLOCKS.get("lavender").get("stained_glass").get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.DYED_BLOCKS.get("persimmon").get("stained_glass").get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.DYED_BLOCKS.get("cherenkov").get("stained_glass").get(), RenderType.getTranslucent());
     }
 }
--- src/main/java/cofh/dyenamics/core/init/BlockInit.java
+++ src/main/java/cofh/dyenamics/core/init/BlockInit.java
@@ -7,6 +7,7 @@ import java.util.function.Supplier;
 import cofh.dyenamics.Dyenamics;
 import cofh.dyenamics.common.blocks.DyenamicCarpetBlock;
 import cofh.dyenamics.common.blocks.DyenamicFlammableBlock;
+import cofh.dyenamics.common.blocks.DyenamicStainedGlassBlock;
 import cofh.dyenamics.core.util.DyenamicDyeColor;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
@@ -25,16 +26,16 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class BlockInit {
-	
+
 	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Dyenamics.MOD_ID);
 	public static final Map<String, Map<String, RegistryObject<Block>>> DYED_BLOCKS = new HashMap<>();
-	
+
 	public static void register() {
 		for (DyenamicDyeColor color : DyenamicDyeColor.dyenamicValues()) {
 			registerDyeBlocks(color);
 		}
 	}
-	
+
 	public synchronized static void registerDyeBlocks(DyenamicDyeColor color) {
 		String colorName = color.getString();
 		int light = color.getLightValue();
@@ -42,15 +43,16 @@ public class BlockInit {
 		DyeColor analogue = color.getAnalogue();
 		final Map<String, RegistryObject<Block>> blocks = new HashMap<>();
 		DYED_BLOCKS.put(colorName, blocks);
-		
+
 		registerBlockAndItem(colorName, "terracotta", blocks, ItemGroup.BUILDING_BLOCKS, () -> new Block(AbstractBlock.Properties.create(Material.ROCK, mapColor).setRequiresTool().hardnessAndResistance(1.25F, 4.2F).setLightLevel((state) -> {return light;})));
 		registerBlockAndItem(colorName, "glazed_terracotta", blocks, ItemGroup.DECORATIONS, () -> new GlazedTerracottaBlock(AbstractBlock.Properties.create(Material.ROCK, analogue).setRequiresTool().hardnessAndResistance(1.4F).setLightLevel((state) -> {return light;})));
 		final RegistryObject<Block> concrete = registerBlockAndItem(colorName, "concrete", blocks, ItemGroup.BUILDING_BLOCKS, () -> new Block(AbstractBlock.Properties.create(Material.ROCK, analogue).setRequiresTool().hardnessAndResistance(1.8F).setLightLevel((state) -> {return light;})));
 		registerBlockAndItem(colorName, "concrete_powder", blocks, ItemGroup.BUILDING_BLOCKS, () -> new ConcretePowderBlock(concrete.get(), AbstractBlock.Properties.create(Material.SAND, analogue).hardnessAndResistance(0.5F).sound(SoundType.SAND).harvestTool(ToolType.SHOVEL).setLightLevel((state) -> {return light;})));
 		registerBlockAndItem(colorName, "wool", blocks, ItemGroup.BUILDING_BLOCKS, () -> new DyenamicFlammableBlock(AbstractBlock.Properties.create(Material.WOOL, mapColor).hardnessAndResistance(0.8F).sound(SoundType.CLOTH).setLightLevel((state) -> {return light;})));
 		registerBlockAndItem(colorName, "carpet", blocks, ItemGroup.DECORATIONS, () -> new DyenamicCarpetBlock(color, AbstractBlock.Properties.create(Material.CARPET, mapColor).hardnessAndResistance(0.1F).sound(SoundType.CLOTH).setLightLevel((state) -> {return light;})));
+		registerBlockAndItem(colorName, "stained_glass", blocks, ItemGroup.BUILDING_BLOCKS, () -> new DyenamicStainedGlassBlock(color, AbstractBlock.Properties.create(Material.GLASS, mapColor).hardnessAndResistance(0.3F).sound(SoundType.GLASS).notSolid().setLightLevel((state) -> {return light;})));
 	}
-	
+
 	public synchronized static RegistryObject<Block> registerBlockAndItem(String color, String block, Map<String, RegistryObject<Block>> blockMap, ItemGroup group, Supplier<Block> supplier) {
 		String name = color + "_" + block;
 		RegistryObject<Block> blockRegistryObject = BLOCKS.register(name, supplier);
@@ -58,5 +60,5 @@ public class BlockInit {
 		blockMap.put(block, blockRegistryObject);
 		return blockRegistryObject;
 	}
-	
+
 }
